5882f8d243052ca490696599a87d4cee429756b9
IshikawaHiroki314 <102946554+IshikawaHiroki314@users.noreply.github.com>
2022-05-15 14:04:44 +0900

    Initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    private float visiblePosZ = -6.5f;
+
+    //private GameObject scoreObject = null;
+    private Text S_text;
+    private int score = 0;
+    private GameObject gameoverText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.scoreObject = GameObject.Find("ScoreText");
+        this.S_text = GameObject.Find("ScoreText").GetComponent<Text>();
+        this.gameoverText = GameObject.Find("GameOverText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        S_text.text = "Score:" + score.ToString();
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(tag == "SmallStarTag")
+        {
+            this.score += 5;
+        }
+        else if(tag == "LargeStarTag")
+        {
+            this.score += 20;
+        }
+        else if(tag == "SmallCloudTag" || tag == "LargeCloudTag")
+        {
+            this.score += 10;
+        }
+    }
+}
+
